18ac824e38317cf416a0e04c94f5ad2164b41626
Yarnottc7892 <yarnottc7892@my.uwstout.edu>
2020-02-21 19:34:43 -0600

    Comments

--- impact/lib/game/entities/CoffeeToffee.js
+++ impact/lib/game/entities/CoffeeToffee.js
@@ -8,17 +8,22 @@ ig.module(
 )
 .defines(function(){
 
+    //Class Definition
     EntityCoffeeToffee = ig.Entity.extend({
+        //Size of the hit box
         size: {x: 32, y:32},
-        name: "High",
+        //Animation Sheet
         animSheet: new ig.AnimationSheet('media /Coffee_Toffee.png',32,32),
-        type: ig.Entity.TYPE.B,
+        //Defines what objects can interact with this one. Set in the level editor
         target: {},
+        //Text that the character output. Set in the level editor
         text: '',
+        //Define the collision type
         collides: ig.Entity.COLLIDES.FIXED,
-
+        //Order that the object is drawn
         zIndex: 0,
 
+        //Function used to set default values
         init: function(x, y , settings){
             this.parent(x,y,settings);
             this.addAnim('highlight',.5,[0, 2]);
@@ -27,17 +32,24 @@ ig.module(
             this.currentAnim = this.anims.idle;
         },
 
+        //Function that runs every frame
         update: function(){
 
+            //For every target assigned to the object
             for(var t in this.target){
+                //Find the entity associated to the target
                 var ent = ig.game.getEntityByName(this.target[t])
+                //If the target is the player
                 if(ent && ent instanceof EntityPlayer){
+                    //Get the distance between the target and our object
                     var dist = this.distanceTo(ent);
+                    //If it is less than 50 highlight the object 
                     if(dist < 50) {
                         if(this.currentAnim == this.anims.idle){
                             this.currentAnim = this.anims.highlight;
                         }
                     }
+                    //Other wise set animation to idle
                     else{
                         if(this.currentAnim == this.anims.highlight){
                             this.currentAnim = this.anims.idle;
@@ -48,16 +60,13 @@ ig.module(
             }
 
 
-            
-            if(ig.input.pressed('action') && this.currentAnim == this.anims.highlight && ig.game.frozen == false){
+            //If the object is hilighted and E is pressed
+            if(ig.input.pressed('action') && this.currentAnim == this.anims.highlight && ig.game.frozen == false){            
 
-                for(var t in this.target){
-                    if(ent && ent instanceof EntityPlayer){
-                        ig.game.frozen = true;
-                        ig.log(ig.game.frozen);
-                    }
-                }
-            
+                //Freeze the player movement
+                ig.game.frozen = true;
+
+                //Spawn the text entity to output text
                 var textBox = ig.game.spawnEntity(EntityTextBox, 32, ig.system.height - 32);
                 textBox.setText(this.text);
 
--- impact/lib/game/entities/matchingCard.js
+++ impact/lib/game/entities/matchingCard.js
@@ -6,14 +6,24 @@ ig.module(
 )
 .defines(function(){
    
+    //Class Definition
     EntityMatchingCard = ig.Entity.extend({
+        //Animation Sheet
         animSheet: new ig.AnimationSheet('media/TestCards.png',64,64),
+        //Determines the layer is drawn on
         zIndex: 5,
+        //Number the card is assigned
         matchNumber: 0,
+        //Defines a offset in the x direction, so the cards are evenly spaced
         coordX: 0,
+        //Defines a offset in the y direction, so the cards are evenly spaced
         coordY: 0,
+        //Define the size of the hit box
         size: {x: 64, y: 64},
+        //Intial position of the card. Gets changed when the card spawns
         pos: {x: 32, y: 32},
+
+        //Function that changes the image of card based off the matchNumber
         changeFrame: function(){
             switch(this.matchNumber){
                 case 0:
@@ -36,7 +46,9 @@ ig.module(
                     break;
             }
         },
+        //Functions used to initialize the values of the card
         init:function(){
+            //Creating the animations using the animation sheet
             this.addAnim( 'idle', 1, [6]);
             this.addAnim( 'square1', 1, [0]);
             this.addAnim( 'square2', 1, [1]);
@@ -45,8 +57,12 @@ ig.module(
             this.addAnim( 'star1', 1, [4]);
             this.addAnim( 'star2', 1, [5]);
 
+            //Set the current animation to idle
             this.currentAnim = this.anims.idle
         },
+
+        //Function that is run every frame. 
+        //Used to draw the card in the right position when it is spawned
         draw: function(){
             this.parent();
             var offsetX = ig.game.screen.x;
@@ -54,15 +70,23 @@ ig.module(
             this.pos.x = offsetX + 128 + this.coordX * 96;
             this.pos.y = offsetY + 40 + this.coordY * 80;
         },
+
+        //Tick Function that is run every frame
         update: function(){
+
+            //Get the position of the mouse
             x = (ig.input.mouse.x + ig.game.screen.x);
             y = (ig.input.mouse.y + ig.game.screen.y);
+
+            //Event called when the mouse is click and the position overlaps with the card
             if(ig.input.pressed('click') && x >= this.pos.x && x <= this.pos.x + this.size.x && y >= this.pos.y && y <= this.pos.y + this.size.y && this.currentAnim == this.anims.idle){
 
+                //Check to see if another card has been clicked
                 if(ig.game.card1 == null){
                     ig.game.card1 = this
                     this.changeFrame();
                 }
+                //Otherwise we set to the second card
                 else if(ig.game.card2 == null){
                     ig.game.card2 = this
                     this.changeFrame();
